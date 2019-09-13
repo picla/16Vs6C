7b6201547f9b867e099ec1d1d04eff8078f8fb76
picla <pieter.clauw@gmi.oeaw.ac.at>
2019-09-13 14:48:44 +0200

    updated walltime according used walltime

--- PlantGrowth/polynomialModel_clstr_V3_corCAR1.sh
+++ PlantGrowth/polynomialModel_clstr_V3_corCAR1.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-#SBATCH --time=14-00:00:00
+#SBATCH --time=30:00:00
 #SBATCH --output Logs/ultimate_QandD_poly_allReps_V3_corCAR1_correct.log
 #SBATCH --mem=60GB
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
-#SBATCH --qos=long
+#SBATCH --qos=medium
+
+# used walltime=23:42
 
 module load openmpi/3.1.1-gcc-7.3.0-2.30
 module load r/3.5.1-foss-2018b
