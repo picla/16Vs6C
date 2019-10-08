97eba1be5a95676b0ca206c3e94e4582d4abec6e
picla <pieter.clauw@gmi.oeaw.ac.at>
2019-10-08 10:29:12 +0200

    polynomial models with experiment added as fixed effect

---
+++ PlantGrowth/polynomialModel_clstr_V3_corCAR1_expFixed.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=7-00:00:00
+#SBATCH --output Logs/ultimate_QandD_poly_allReps_V3_corCAR1_expFixed.log
+#SBATCH --mem=120GB                                                                                                  
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --qos=long
+
+module load openmpi/3.1.1-gcc-7.3.0-2.30
+module load r/3.5.1-foss-2018b
+
+Rscript --vanilla ${HOME}/GitRepos/16Vs6C/PlantGrowth/polynomialModel_clstr_V3_corCAR1_expFixed.r
+
+
