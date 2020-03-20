1379d5fdd5a53e817c9eaad748706941631610a9
picla <pieter.clauw@gmi.oeaw.ac.at>
2020-03-20 22:31:46 +0100

    updated STAR index building for pseugenomes to CBE

---
+++ Transcriptome/mapping/STAR_index_pseudoG.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# SLURM #
+#SBATCH --output Logs/pseudoG_STARidx_%A_%a.log
+#SBATCH --mem=40GB
+#SBATCH --cpus-per-task=4
+#SBATCH --array=1-7
+
+# MODULES #
+ml star/2.7.1a-foss-2018b
+
+# DATA #
+i=$SLURM_ARRAY_TASK_ID
+mainDir=/scratch-cbe/users/pieter.clauw/16vs6/Data/Genome/
+pseudoGs=${mainDir}pseudogenomes.txt
+FASTA=$(sed -n ${i}p $pseudoGs)
+Indices=${FASTA/.fasta/_STAR_idx/}
+Araport11GTF=${mainDir}Araport11_GFF3_genes_transposons.201606.ChrM_ChrC_FullName.gtf
+# old
+# TAIR10GFF=$HOME/Data/TAIR10/Arabidopsis_thaliana.TAIR10.41.gtf
+
+# PARAMETERS #
+cores=4
+
+# PREPARATION #
+mkdir -p $Indices
+
+# GENOME INDICES #
+
+STAR \
+--runThreadN $cores \
+--runMode genomeGenerate \
+--genomeDir $Indices \
+--genomeFastaFiles $FASTA \
+--sjdbGTFfile $Araport11GTF \
+--sjdbOverhang 124
+
+
+
