f360c156864e9936e7dfd38d18a665e317d16104
picla <pieter.clauw@gmi.oeaw.ac.at>
2020-03-20 15:26:45 +0100

    updated to run on CBE

--- Transcriptome/mapping/make_pseudogenome_fasta.sh
+++ Transcriptome/mapping/make_pseudogenome_fasta.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 
-# PBS #
-#PBS -N makePseudoGenomes
-#PBS -P cegs
-#PBS -j oe
-#PBS -o Logs/makePseudeGenomes_^array_index^.log
-#PBS -l walltime=12:00:00,mem=10GB
-#PBS -J 1-3
+# SLURM #
+#SBATCH --output Logs/makePseudeGenomes_%A_%a.log
+#SBATCH --mem=10GB
+#SBATCH --array=1-7
 
 # MODULES #
-ml Python/3.6.4-foss-2018a
+ml python/3.6.6-foss-2018b
 # copy script from /projects/cegs/6vs16/Scripts/make_pseudogenome_fasta.py
-PSEUDOGENIZE=${WORK}Transcriptome/6vs16/Scripts/make_pseudogenome_fasta.py 
+PSEUDOGENIZE=~/GitRepos/16Vs6C/Transcriptome/mapping/make_pseudogenome_fasta.py 
 
 # DATA #
-i=$PBS_ARRAY_INDEX
-mainDir=${WORK}Transcriptome/6vs16/Data/Genome/
-FASTA=${mainDir}TAIR10_genome.fasta
-VCFlst=${mainDir}vcf_for_pseudoGenome.txt
-VCF=${mainDir}$(sed -n ${i}p $VCFlst)
-OUT=${VCF/intersection/pseudoTAIR10}
-OUT=${OUT/.vcf/.fasta}
+i=$SLURM_ARRAY_TASK_ID
+mainDir=/scratch-cbe/users/pieter.clauw/16vs6/Data/Genome/
+FASTA=${mainDir}TAIR10_chromosomes.fas
+VCFlst=${mainDir}vcf_for_pseudogenome.txt
+VCF=$(sed -n ${i}p $VCFlst)
+OUT=${VCF/1001genomes_snp-short-indel_only_ACGTN/pseudogenomes/pseudoTAIR10}
+OUT=${OUT/.vcf.gz/.fasta}
 
 # MAKE PSEUDO GENOMES #
 python $PSEUDOGENIZE -O $OUT $FASTA $VCF
