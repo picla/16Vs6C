9312dc658d3b3226bcae3e6a8c8df6362ce05c45
picla <pieter.clauw@gmi.oeaw.ac.at>
2020-03-20 21:56:05 +0100

    copy raw bamfiles to scratch

---
+++ Transcriptome/mapping/stage_bamfiles.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+# stage rawdata bamfiles from rawdata repository to scratch-cbe
+
+# SLURM #
+SBATCH --output Logs/stage_bamfiles.log
+
+SAMPLES=/groups/nordborg/projects/cegs/16Vs6C/Data/Transcriptome/RawData/samples.txt
+BAMFILES=($(awk 'NR>1 {print $10}' $SAMPLES))
+
+TARGET=/scratch-cbe/users/pieter.clauw/16vs6/Data/Transcriptome/BAMraw/
+
+for BAM in ${BAMFILES[@]}; do
+    cp -v $BAM $TARGET
+done
+
+
