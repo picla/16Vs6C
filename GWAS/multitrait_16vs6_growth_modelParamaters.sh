780589578ea68f52bacf47fc0a869311869bae96
picla <pieter.clauw@gmi.oeaw.ac.at>
2020-03-31 11:49:55 +0200

    corrected input file

--- GWAS/multitrait_16vs6_growth_modelParamaters.sh
+++ GWAS/multitrait_16vs6_growth_modelParamaters.sh
@@ -2,7 +2,7 @@
 
 # SLURM
 #SBATCH --mem=20GB
-#SBATCH --output=Logs/LGR_16vs6.log
+#SBATCH --output=Logs/multitrait_16vs6_growth_%A_%a.log
 #SBATCH --array=2-7:2
 
 # ENVIRONMENT #
@@ -13,13 +13,13 @@ conda activate ~/.conda/envs/limix
 # DATA #
 i=$SLURM_ARRAY_TASK_ID
 j=$((i+1))
-allphenotypes=/groups/nordborg/user/pieter.clauw/Documents/Experiments/UltimateQandD/Data/Growth/GWAS/growthPheno.csv
+allphenotypes=/groups/nordborg/user/pieter.clauw/Documents/Experiments/UltimateQandD/Results/Growth/nonlinear/modelParameters.csv
 trait=$(head -n 1 $allphenotypes | cut -d',' -f $i,$j)
 trait=${trait/,/_}
 
 PHENO=/tmp/${trait}.csv
-GENO=/scratch-cbe/users/pieter.clauw/genotypes_for_pygwas/1.0.0/1001genomes/
-OUT=/scratch-cbe/users/pieter.clauw/16vs6/Results/GWAS/MultiTrait/
+GENO=/scratch-cbe/users/pieter.clauw/common_data/genotypes_for_pygwas/1.0.0/1001genomes/
+OUT=/scratch-cbe/users/pieter.clauw/16vs6/Results/GWAS/multiTrait/
 
 awk -F ',' -v OFS=',' -v pheno1="$i" -v pheno2="$j" '{print $1,$pheno1,$pheno2}' $allphenotypes > $PHENO
 
